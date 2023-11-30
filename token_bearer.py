7326e9336d1c697b14b0784bf9ad268c9cbc0a8a
saad415 <saad415415@gmail.com>
2023-11-30 22:43:18 +0100

    added token bearer

---
+++ token_bearer.py
@@ -0,0 +1,33 @@
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Set your Spotify API credentials
+
+client_id = "484f223094b54d77a0b836e982d81799"
+client_secret = "791a80ce2748414bb9b7e455c3d15ccc"
+redirect_uri = 'https://accounts.spotify.com/authorize'
+# Initialize the Spotify client with authentication
+sp_oauth = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope='user-library-read')
+
+# Get the authorization URL
+auth_url = sp_oauth.get_authorize_url()
+
+# Open the authorization URL in a web browser
+import webbrowser
+webbrowser.open(auth_url)
+
+# After authorizing your app, Spotify will redirect to the specified redirect_uri with the authorization code.
+# Extract the authorization code from the URL. You can manually copy it from the address bar.
+# For example, if the URL is http://localhost:8080/callback?code=YOUR_CODE, extract YOUR_CODE.
+
+# Set the authorization code here
+authorization_code = 'YOUR_CODE'
+
+# Request the Bearer Token using the authorization code
+token_info = sp_oauth.get_access_token(authorization_code)
+
+# The Bearer Token is now stored in the token_info dictionary
+bearer_token = token_info['access_token']
+
+print(f'Spotify Bearer Token: {bearer_token}')
