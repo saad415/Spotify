a52c2e594cf2ccfd429a550782f101659d0fc094
Saad Ahmad <saad415415@gmail.com>
2023-12-16 14:47:55 +0100

    Update token_bearer.py

--- token_bearer.py
+++ token_bearer.py
@@ -4,8 +4,8 @@ from spotipy.oauth2 import SpotifyOAuth
 
 # Set your Spotify API credentials
 
-client_id = "484f223094b54d77a0b836e982d81799"
-client_secret = "791a80ce2748414bb9b7e455c3d15ccc"
+client_id = ""
+client_secret = ""
 redirect_uri = 'https://accounts.spotify.com/authorize'
 # Initialize the Spotify client with authentication
 sp_oauth = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope='user-library-read')
