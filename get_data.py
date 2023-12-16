15fe1a6318eddeb9f8c2c70fce64040e43220cf6
saad415 <saad415415@gmail.com>
2023-12-16 12:15:10 +0100

    Complete python file to get data from s3 bucket into power bi

--- get_data.py
+++ get_data.py
@@ -1,32 +1,54 @@
 import boto3
 import pandas as pd
 import io
-# AWS credentials (configure these with your own values)
+
+def download_csv_files_from_s3(aws_access_key_id, aws_secret_access_key, s3_bucket_name, file_keys):
+    # Initialize an S3 client
+    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    # Create a dictionary to store DataFrames
+    dataframes = {}
+
+    # Iterate over the list of file keys and download each CSV file
+    for file_key in file_keys:
+        try:
+            response = s3.get_object(Bucket=s3_bucket_name, Key=file_key)
+            print("bucket_name: ", s3_bucket_name, "\nfile_key: ", file_key)
+            content = response['Body'].read()
+
+            # Read the CSV content into a Pandas DataFrame
+            df = pd.read_csv(io.BytesIO(content))
+
+            # Store the DataFrame in the dictionary with the file key as the key
+            dataframes[file_key] = df
+
+        except Exception as e:
+            print(f"An error occurred for file key '{file_key}': {str(e)}")
+
+    return dataframes
+
+# Example usage:
 aws_access_key_id = 'AKIAUTEANXGB2VM2JSPP'
 aws_secret_access_key = 'QfrguEBd0/8rmXctChoj+oO6WyaZLUZvhU2Jlcht'
 s3_bucket_name = 'saad-spotify'
-s3_file_key = 'track_audio_features.csv'  # Update to point to the CSV file in your S3 bucket
+file_keys = ['track_audio_features.csv', 'user_playlists.csv', 'all_user_tracks.csv', 'user_artists.csv', 'user_profile.csv']  # List of S3 object keys
 
-# Initialize an S3 client
-s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+dataframes = download_csv_files_from_s3(aws_access_key_id, aws_secret_access_key, s3_bucket_name, file_keys)
 
-# Specify the S3 bucket and file you want to retrieve
-bucket_name = s3_bucket_name
-file_key = s3_file_key
+# Access the downloaded DataFrames by their file keys
+if 'track_audio_features.csv' in dataframes:
+    track_audio_features = dataframes['track_audio_features.csv']
+    # Work with df1
 
-# Download the CSV file from S3
-try:
-    response = s3.get_object(Bucket=bucket_name, Key=file_key)
-    content = response['Body'].read()
+if 'user_playlists.csv' in dataframes:
+    user_playlists = dataframes['user_playlists.csv']
+    # Work with df2
 
-    # Read the CSV content into a Pandas DataFrame
-    df = pd.read_csv(io.BytesIO(content))
+if 'all_user_tracks.csv' in dataframes:
+    all_user_tracks = dataframes['all_user_tracks.csv']
 
-    # Now you can work with the DataFrame 'df' containing the CSV data
-    # Optionally, you can save the DataFrame as a new CSV file locally
-    local_csv_file = 'downloaded_data.csv'  # Specify the local file path
-    df.to_csv(s3_file_key, index=False)
+if 'user_profile.csv' in dataframes:
+    user_profile = dataframes['user_profile.csv']
 
-   # print(f"Data saved to '{local_csv_file}'")
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+if 'user_artists.csv' in dataframes:
+    user_artists = dataframes['user_artists.csv']
